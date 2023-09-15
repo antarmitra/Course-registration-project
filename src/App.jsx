bdbbc625896ae9c9ec8033089aacff935b3a1753
AntarProgrammer <antarmitra33@gmail.com>
2023-09-15 12:11:20 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h3 className='text-5xl font-bold text-center mt-10'>Course Registration</h3>
+      
+    </>
+  )
+}
+
+export default App
